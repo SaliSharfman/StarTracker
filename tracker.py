2df29a594faa906d400b4d3a96dc377a234e4fa6
segev955 <segev95@gmail.com>
2023-04-20 11:00:15 +0300

    add id

--- tracker.py
+++ tracker.py
@@ -3,6 +3,7 @@ import os
 import cv2
 import numpy as np
 
+
 def make_dirs(dirs=[]):
     path = os.path.join(os.getcwd())
     for dir in dirs:
@@ -10,6 +11,7 @@ def make_dirs(dirs=[]):
             os.mkdir(f'{path}/{dir}')
         except:
             pass
+
 
 def makeCsv(l, folder='logs', filename="no_name"):
     csvname = filename.split('.')[0] + '.csv'
@@ -33,20 +35,21 @@ def detect_img(filename, dir_src, dir_dest, dir_log):
         print(f"cant open file {filename}")
         return
     cv2.namedWindow(f'{filename} Stars', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow(f'{filename} Stars', 800, 600)
+    cv2.resizeWindow(f'{filename} Stars', 1024, 768)
     cv2.imshow(f'{filename} Stars', img_copy)
-    cv2.waitKey(0)
+    cv2.waitKey(1)
     cv2.destroyAllWindows()
 
     print(f"start detecting {filename}")
-    stars = [['x', 'y', 'r', 'b']]
+    stars = [['id', 'x', 'y', 'r', 'b']]
     gray = cv2.cvtColor(img_copy, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (3, 3), 0)
     avg = np.mean(gray)
     _, thresh = cv2.threshold(blur, avg * 1.5, 255, cv2.THRESH_BINARY)
     num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(thresh)
     print(f"found {num_labels} labels")
-    percents=10
+    percents = 10
+    id = 1
     for i in range(1, num_labels):
         if percents < (100 * i) / num_labels:
             print(f'{percents}% detected')
@@ -57,17 +60,20 @@ def detect_img(filename, dir_src, dir_dest, dir_log):
         r = int((stats[i, cv2.CC_STAT_WIDTH] + stats[i, cv2.CC_STAT_HEIGHT]) / 4)
         if r >= min_radius:
             b = cv2.mean(gray, cv2.UMat(mask))[0]
-            stars.append((x, y, r, b))
+            stars.append((id, x, y, r, b))
             cv2.circle(img_copy, (int(x), int(y)), r + 5, (0, 255, 0), 2)
-    print(f'100% detected\n{len(stars)-1} stars detected')
-    makeCsv(stars,folder=dir_log, filename=filename)
+            cv2.putText(img_copy, f"{id}", (int(x) - 20, int(y) - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 255), 2)
+            id += 1
+    print(f'100% detected\n{len(stars) - 1} stars detected')
+    makeCsv(stars, folder=dir_log, filename=filename)
     cv2.imwrite(f'{dir_dest}\detected_{filename}', img_copy)
     print(f'detected_{filename} saved.')
     cv2.namedWindow(f'{filename} Detected Stars', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow(f'{filename} Detected Stars', 800, 600)
+    cv2.resizeWindow(f'{filename} Detected Stars', 1024, 768)
     cv2.imshow(f'{filename} Detected Stars', img_copy)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
 
 if __name__ == '__main__':
     dir_src = 'images'
@@ -77,12 +83,10 @@ if __name__ == '__main__':
     dataset = os.listdir(dir_src)
     print("images:")
     for i in range(len(dataset)):
-        print(f"{i+1}: {dataset[i]}")
+        print(f"{i + 1}: {dataset[i]}")
     str_in = input("input the number of the image for detecting, to detect all input <all>\n")
-    if str_in =='all':
+    if str_in == 'all':
         for i in dataset:
             detect_img(filename=i, dir_src=dir_src, dir_dest=dir_dest, dir_log=dir_log)
-    elif int(str_in)-1 in range(len(dataset)):
-        detect_img(filename=dataset[int(str_in)-1], dir_src=dir_src, dir_dest=dir_dest, dir_log=dir_log)
-
-
+    elif int(str_in) - 1 in range(len(dataset)):
+        detect_img(filename=dataset[int(str_in) - 1], dir_src=dir_src, dir_dest=dir_dest, dir_log=dir_log)
